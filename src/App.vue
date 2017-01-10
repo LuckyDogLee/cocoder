ea8d3cf2655b8a510260543046b8aa08147338c7
LuckyDogLee <luckydoglee@163.com>
2017-01-10 13:50:09 +0800

    add axios & change logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <template>
-  <h1>Hello Cocoder!</h1>
+  <div id="app">
+    <h1>Hello Cocoder!</h1>
+    <button @click="load">Load</button>
+    <span>{{ cnode.ask }}</span>
+  </div>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  let cnode = {
+    ask: 'loading'
+  };
+
+  export default {
+    data: function() {
+      return {
+        cnode: cnode
+      }
+    },
+    methods: {
+      load: function() {
+        axios.get('/api/v1/topics?page=168&limit=40&tab=ask')
+          .then(function (response) {
+             console.log(response);
+             cnode.ask = response;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    }
+  }
+</script>
 
 <style>
   h1 {
